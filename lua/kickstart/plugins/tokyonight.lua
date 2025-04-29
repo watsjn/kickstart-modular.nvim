18a02d74b5a258fe25003ed0f60d7fc9977ba171
watsjn <48692528+watsjn@users.noreply.github.com>
2025-04-29 17:30:19 +0300

    Update tokyonight.lua

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -9,7 +9,7 @@ return {
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
-        style = 'moon',
+        style = 'night',
         styles = {
           comments = { italic = false }, -- Disable italics in comments
           functions = { bold = true },
@@ -20,7 +20,7 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-moon'
     end,
   },
 }
