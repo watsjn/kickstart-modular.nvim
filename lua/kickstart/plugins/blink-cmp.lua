8583a6919311e244bbddf62a10fb8b44fc8d2fa2
watsjn <48692528+watsjn@users.noreply.github.com>
2025-07-23 00:38:02 +0300

    Update blink-cmp.lua
    ripgrep source, completion format changed

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -31,6 +31,7 @@ return {
         opts = {},
       },
       'folke/lazydev.nvim',
+      'mikavilpas/blink-ripgrep.nvim',
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
@@ -70,6 +71,19 @@ return {
       },
 
       completion = {
+        menu = {
+          border = nil,
+          scrolloff = 1,
+          scrollbar = false,
+          draw = {
+            columns = {
+              { 'kind_icon' },
+              { 'label', 'label_description', gap = 1 },
+              { 'kind' },
+              { 'source_name' },
+            },
+          },
+        },
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = false, auto_show_delay_ms = 500 },
@@ -78,9 +92,121 @@ return {
       },
 
       sources = {
-        default = { 'buffer', 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'buffer', 'lsp', 'path', 'snippets', 'lazydev', 'ripgrep' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          ripgrep = {
+            module = 'blink-ripgrep',
+            name = 'Ripgrep',
+            -- the options below are optional, some default values are shown
+            ---@module "blink-ripgrep"
+            ---@type blink-ripgrep.Options
+            opts = {
+              -- For many options, see `rg --help` for an exact description of
+              -- the values that ripgrep expects.
+
+              -- the minimum length of the current word to start searching
+              -- (if the word is shorter than this, the search will not start)
+              prefix_min_len = 3,
+
+              -- The number of lines to show around each match in the preview
+              -- (documentation) window. For example, 5 means to show 5 lines
+              -- before, then the match, and another 5 lines after the match.
+              context_size = 5,
+
+              -- The maximum file size of a file that ripgrep should include in
+              -- its search. Useful when your project contains large files that
+              -- might cause performance issues.
+              -- Examples:
+              -- "1024" (bytes by default), "200K", "1M", "1G", which will
+              -- exclude files larger than that size.
+              max_filesize = '1M',
+
+              -- Specifies how to find the root of the project where the ripgrep
+              -- search will start from. Accepts the same options as the marker
+              -- given to `:h vim.fs.root()` which offers many possibilities for
+              -- configuration. If none can be found, defaults to Neovim's cwd.
+              --
+              -- Examples:
+              -- - ".git" (default)
+              -- - { ".git", "package.json", ".root" }
+              project_root_marker = '.git',
+
+              -- Enable fallback to neovim cwd if project_root_marker is not
+              -- found. Default: `true`, which means to use the cwd.
+              project_root_fallback = true,
+
+              -- The casing to use for the search in a format that ripgrep
+              -- accepts. Defaults to "--ignore-case". See `rg --help` for all the
+              -- available options ripgrep supports, but you can try
+              -- "--case-sensitive" or "--smart-case".
+              search_casing = '--ignore-case',
+
+              -- (advanced) Any additional options you want to give to ripgrep.
+              -- See `rg -h` for a list of all available options. Might be
+              -- helpful in adjusting performance in specific situations.
+              -- If you have an idea for a default, please open an issue!
+              --
+              -- Not everything will work (obviously).
+              additional_rg_options = {},
+
+              -- When a result is found for a file whose filetype does not have a
+              -- treesitter parser installed, fall back to regex based highlighting
+              -- that is bundled in Neovim.
+              fallback_to_regex_highlighting = true,
+
+              -- Absolute root paths where the rg command will not be executed.
+              -- Usually you want to exclude paths using gitignore files or
+              -- ripgrep specific ignore files, but this can be used to only
+              -- ignore the paths in blink-ripgrep.nvim, maintaining the ability
+              -- to use ripgrep for those paths on the command line. If you need
+              -- to find out where the searches are executed, enable `debug` and
+              -- look at `:messages`.
+              ignore_paths = {},
+
+              -- Any additional paths to search in, in addition to the project
+              -- root. This can be useful if you want to include dictionary files
+              -- (/usr/share/dict/words), framework documentation, or any other
+              -- reference material that is not available within the project
+              -- root.
+              additional_paths = {},
+
+              -- Keymaps to toggle features on/off. This can be used to alter
+              -- the behavior of the plugin without restarting Neovim. Nothing
+              -- is enabled by default. Requires folke/snacks.nvim.
+              toggles = {
+                -- The keymap to toggle the plugin on and off from blink
+                -- completion results. Example: "<leader>tg" ("toggle grep")
+                on_off = nil,
+
+                -- The keymap to toggle debug mode on/off. Example: "<leader>td" ("toggle debug")
+                debug = nil,
+              },
+
+              -- Features that are not yet stable and might change in the future.
+              -- You can enable these to try them out beforehand, but be aware
+              -- that they might change. Nothing is enabled by default.
+              future_features = {
+                backend = {
+                  -- The backend to use for searching. Defaults to "ripgrep".
+                  -- Available options:
+                  -- - "ripgrep", always use ripgrep
+                  -- - "gitgrep", always use git grep
+                  -- - "gitgrep-or-ripgrep", use git grep if possible, otherwise
+                  --   ripgrep
+                  use = 'ripgrep',
+                  -- Whether to set up custom highlight-groups for the icons used
+                  -- in the completion items. Defaults to `true`, which means
+                  -- this is enabled.
+                  customize_icon_highlight = true,
+                },
+              },
+
+              -- Show debug information in `:messages` that can help in
+              -- diagnosing issues with the plugin.
+              debug = false,
+            },
+          },
         },
       },
 
